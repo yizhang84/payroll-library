7effe68bab610a288086b59f5011d36005e33f82
yizhang84 <yi.zhang@bbc.co.uk>
2023-11-08 16:22:35 +0000

    lib-resolver second commit

---
+++ src/main/java/com/yi/payrolllibresolver/PayrollLibResolverApplication.java
@@ -0,0 +1,13 @@
+package com.yi.payrolllibresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollLibResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollLibResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yi/payrolllibresolver/PayrollLibResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yi.payrolllibresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollLibResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
